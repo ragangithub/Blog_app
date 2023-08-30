212c25e4433d1d732a0a9c0a5d204c5f4d871474
ragan <raganalemu3@gmail.com>
2023-08-30 15:50:00 +0300

    Add autherization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to user_posts_path(@post.author_id)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :show
+    end
+  end
+
   private
 
   def post_params
