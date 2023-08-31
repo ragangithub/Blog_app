7413feedf31282b8def9dbe03e342b23eafd9954
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-08-31 07:54:19 +0000

    Update the ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,15 @@
 class Ability
   include CanCan::Ability
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+        can :manage, :all
+    else
+      can :destroy, Post do |post|
+        post.author == user
+      end
+      can :destroy, Comment do |comment|
+        comment.author == user
+      end
+   end
 end
